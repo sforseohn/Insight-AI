d70129459a3ddccb75256184f08f40d197329a6b
sforseohn <sforseohn@gmail.com>
2023-06-28 02:25:05 +0900

    [FrontEnd] Add Sign up UI

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,9 +1,9 @@
 document.getElementById('signupForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+    var name = document.getElementById('signup_name').value;
+    var email = document.getElementById('signup_email').value;
+    var password = document.getElementById('signup_password').value;
 
 		let user = {
 				name: name,
@@ -26,10 +26,20 @@ document.getElementById('signupForm').addEventListener('submit', function(event)
             console.log('Signup GOOOOD');
             console.log(JSON.stringify(user));
             alert(data.message);
-            window.location.href = '/login.html';
+            window.location.href = '/';
         }
     })
     .catch((error) => {
         console.error('Error:', error);
     });
 });
+
+function signupPopup() {
+    var popup = document.getElementById("signup__bg");
+    popup.classList.add("show");
+}
+
+  function signupPopdown() {
+    var popup = document.getElementById("signup__bg");
+    popup.classList.remove("show");
+}
